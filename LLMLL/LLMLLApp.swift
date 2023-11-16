4fe4718562e87d41508bea9d6f8d90e765430283
allswellthatsmaxwell <allswellthatsmaxwell9@gmail.com>
2023-11-16 17:58:53 -0600

    Initial Commit

---
+++ LLMLL/LLMLLApp.swift
@@ -0,0 +1,32 @@
+//
+//  LLMLLApp.swift
+//  LLMLL
+//
+//  Created by Maxwell Peterson on 11/16/23.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct LLMLLApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
