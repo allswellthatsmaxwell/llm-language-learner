321202dcdf86a4dda032e9dd386cb61d65be8602
allswellthatsmaxwell <allswellthatsmaxwell9@gmail.com>
2023-11-16 19:34:04 -0600

    got play saved audio working

--- LLMLL/ContentView.swift
+++ LLMLL/ContentView.swift
@@ -10,6 +10,23 @@ import SwiftData
 import AVFoundation
 
 
+func getDocumentsDirectory() -> URL {
+    let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+    return paths[0]
+}
+
+func saveAudioFile(audioData: Data) {
+    let filename = getDocumentsDirectory().appendingPathComponent("savedAudio.mp3")
+
+    do {
+        try audioData.write(to: filename, options: [.atomicWrite, .completeFileProtection])
+        print("Audio saved to \(filename.path)")
+    } catch {
+        print("Failed to save audio: \(error)")
+    }
+}
+
+
 struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
     @Query private var items: [Item]
@@ -18,7 +35,16 @@ struct ContentView: View {
     private var textToSpeechAPI: TextToSpeechAPI
     
     init() {
-        textToSpeechAPI = TextToSpeechAPI(apiKey: "OPENAI_API_KEY")
+        var apiKey: String?
+        if let apiKeyLocal = ProcessInfo.processInfo.environment["OPENAI_API_KEY"] {
+            // Use apiKey safely here
+            print("API Key: \(apiKeyLocal)")
+            apiKey = apiKeyLocal
+        } else {
+            print("Failed to find OPENAI_API_KEY")
+        }
+        textToSpeechAPI = TextToSpeechAPI(apiKey: apiKey ?? "MissingAPIKey")
+
     }
     
     private func playAudio() {
@@ -49,10 +75,10 @@ struct ContentView: View {
                     result in
                     
                     switch result {
-                    case .success(_):
+                    case .success(let audioData):
                         // Handle the received data, e.g., play audio or save to file
                         print("Audio Data Received")
-                        saveAudioFile(audioData: <#T##Data#>)
+                        saveAudioFile(audioData: audioData)
                     case .failure(let error):
                         // Handle any errors
                         print("Error: \(error.localizedDescription)")
@@ -63,24 +89,11 @@ struct ContentView: View {
     }
 }
 
-#Preview {
-    ContentView()
-        .modelContainer(for: Item.self, inMemory: true)
-}
 
-    
-func getDocumentsDirectory() -> URL {
-    let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-    return paths[0]
-}
-
-func saveAudioFile(audioData: Data) {
-    let filename = getDocumentsDirectory().appendingPathComponent("savedAudio.mp3")
-
-    do {
-        try audioData.write(to: filename, options: [.atomicWrite, .completeFileProtection])
-        print("Audio saved to \(filename.path)")
-    } catch {
-        print("Failed to save audio: \(error)")
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+        // .environment(\.modelContext, YourModelContextHere)
+        // Use this if you need to inject any specific environment objects
     }
 }
