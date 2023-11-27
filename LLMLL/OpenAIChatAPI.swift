4769251f2932914f89212c745c33124ef3fc096c
allswellthatsmaxwell <allswellthatsmaxwell9@gmail.com>
2023-11-26 18:08:32 -0600

    fixed extractor prompt

--- LLMLL/OpenAIChatAPI.swift
+++ LLMLL/OpenAIChatAPI.swift
@@ -285,7 +285,7 @@ class ExtractorChatAPI: ChatAPI {
 Extract the Korean Hangul text from the first part of the message the user gives you. Only extract the Hangul word/phrase/sentence/paragraph/text.
 Don't extract any English. Return only the Hangul you extract, with no additional text. No English whatsoever!
 
-If there is no Hangul, return "Sorry, no Korean text found", in English.
+If there is no Hangul, return "Sorry, no Korean text found", in English. If there is some Hangul - any Hangul at all! - make sure to return it.
 """
     }
 }
